066bd95fa3c53cc8e7946e22962e7a85430a2277
Funtimes909 <funtimes90969@gmail.com>
2024-03-13 21:47:24 +1300

    small fixes to gay command

--- commands/games/gay.js
+++ commands/games/gay.js
@@ -14,13 +14,16 @@ module.exports = {
         const gay = Math.trunc(Math.random() * 100)
         const user = ("<@" + interaction.options.getUser('user') + ">")
         if (gay == 0) {
-            await interaction.reply({content: user + " is " + gay + "% Gay! Hetrosexual spotted, proceeding to ban..."})
+            await interaction.reply({content: user + " is " + gay + "% Gay! Hetreosexual spotted, proceeding to ban..."})
         }
         else if (gay == 100) {
-        await interaction.reply({content: user + " is " + gay + "% Gay! You are extremely gay, keep it up"})
+        await interaction.reply({content: user + " is " + gay + "% Gay! You are extremely gay, You are literally a total gayass"})
         }
         else if (gay == 50) {
             await interaction.reply({content: user + " is " + gay + "% Gay! You aren't gay? You're bisexual!"})
+        }
+        else if (gay == 69) {
+            await interaction.reply({content: user + " is " + gay + "% Gay! You want GAY SEX???"})
         }
         else {
             await interaction.reply({content: user + " is " + gay + "% Gay!"})
