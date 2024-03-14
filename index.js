2a9d2f1b918c1a386413c686597604750accd5a8
Funtimes909 <funtimes90969@gmail.com>
2024-03-14 21:32:18 +1300

    Dynamic and auto updating bot activity status (THIS TOOK SO LONG)

--- index.js
+++ index.js
@@ -1,39 +1,40 @@
-// Require the necessary discord.js classes
-const { Client, Collection, Events, GatewayIntentBits, ActivityType } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits, ActivityType, Activity } = require('discord.js');
 const { token } = require('./config.json');
 const fs = require('node:fs');
 const path = require('node:path');
-const { Activity } = require('discord.js');
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
-// Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+function ChangeStatus() {
+	const status = {
+		Watching: ["Helluva Boss", "Hazbin Hotel", "Breaking Bad", "Better Call Saul", "Stranger Things", "The world burn", ""],
+		Playing: ["Minecraft", "Project Zomboid", "ULTRAKILL", "Windows 95", "Windows XP", "Among Us", "OMORI", "Wikipedia", "Visual Studio Code", "Linux", "Firefox", "Hello Kitty Island Adventure"],
+		Listening: ["Arcade Fire", "Daft Punk", "Waterflame", "Queen", "Keygen Church", "Tally Hall", "Heaven Pierce Her", "100 Gecs"]}
+		const keys = Object.keys(status)
+		const type = keys[(Math.trunc(Math.random() * keys.length))]
+		const items = status[type]
+		const currentStatus = items[(Math.trunc(Math.random() * items.length))]
+	if (type == "Listening") {
+		client.user.setActivity(currentStatus, { type: ActivityType.Listening });
+	}
+	else if (type == "Playing") {
+		client.user.setActivity(currentStatus, { type: ActivityType.Playing });
+	}
+	else {
+		client.user.setActivity(currentStatus, { type: ActivityType.Watching });
+	}
+	console.log(`[BOT_STATUS] Bot status has been set to [${type}] [${currentStatus}]`)
+	setInterval(ChangeStatus, 300000)
+}
 
 // When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
 // It makes some properties non-nullable.
 client.once(Events.ClientReady, readyClient => {
-	const watching = ["Helluva Boss", "Hazbin Hotel", "Breaking Bad", "Better Call Saul", "Stranger Things"]
-	const playing = ["Minecraft", "Project Zomboid", "ULTRAKILL", "Windows 95", "Windows XP", "Among Us", "OMORI"]
-	const listening = ["Arcade Fire", "Daft Punk", "Waterflame", "Queen", "Keygen Church"]
-	const currentStatus = (Math.trunc(Math.random() * 3))
-	console.log(currentStatus)
-	if (currentStatus == 1) {
-		const Activity = "Watching"
-	}
-	else if (currentStatus == 2) {
-		const Activity = "Playing"
-	}
-	else if (currentStatus == 3) {
-		const Activity = "Listening"
-	}
-	else {
-		const Activity = "Playing"
-	}
-	client.user.setActivity('Helluva Boss', { type: ActivityType.Activity });
+	ChangeStatus()
 	client.user.setStatus('dnd');
 });
 
